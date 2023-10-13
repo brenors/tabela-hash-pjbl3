96f7c9d911e9d37676fb34ebdc8718b9c8f556a7
Breno Sedrez <breno.sedrez@gmail.com>
2023-10-13 15:13:17 -0300

    criado a tabela hash
    - criado a classe da tabela hash
    - criado uma classe de entrada (teste)

---
+++ src/HashTable.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class HashTable {
+
+    private int size;
+    private LinkedList<Entrada>[] table;
+
+    public HashTable(int size) {
+        this.size = size;
+        this.table = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
+
+    private class Entrada {
+        private int key;
+        private String value;
+
+        public Entrada(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+}
